092166acadd7c3768aa6e3f5825d20dc737a2e94
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-08-08 13:08:20 +0900

    add login

--- lib/agent.rb
+++ lib/agent.rb
@@ -2,13 +2,29 @@ require 'mechanize'
 
 class Agent
   def initialize(user_id, passwd)
-    @agent = login(Mechanize.new)
+    @agent = Login(Mechanize.new, user_id, passwd)
 
   end
 
-  def login(ag)
+  def Login(ag, id, pass)
     page = ag.get('https://www.secure.pixiv.net/login.php')
-    
+    next_page = ClickLogin(page, id, pass)
+
+    if CheckLoginFailer
+
+    end
+
     return ag
+  end
+
+  def ClickLogin(page, id, pass)
+    page.form_with(:class => "login_form") do |form|
+      form.login_pixiv_id = id
+      form.login_password = pass
+    end.submit
+  end
+
+  def CheckLoginFailer(next_page)
+
   end
 end
