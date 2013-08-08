5cea181a69f814d5fa1197a9263749f1a8025175
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-08-08 14:34:19 +0900

    add disable certificate

--- pixiv2.rb
+++ pixiv2.rb
@@ -1,2 +1,9 @@
-require "net/https"
-https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+require "openssl"
+
+def DisableCertificate
+  OpenSSL::SSL.module_eval{ remove_const(:VERIFY_PEER) }
+  OpenSSL::SSL.const_set( :VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE )
+  puts "Warning: Certificate has not been issued. Don't worry :)"
+end
+
+DisableCertificate
